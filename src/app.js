7aa9c22d8bdd97b2eb2bfb79781c70fc92df9c44
Josh Beam <joshua.a.beam@gmail.com>
2015-09-12 17:29:10 -0700

    Adds wildcard access control allow origin header

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ var express = require('express');
 var app = express();
 
 import routes from './routes';
+import headers from './routes/headers.js';
+
+app.use(headers);
 
 app.use('/api/1.0/', routes);
 
---
+++ src/routes/headers.js
@@ -0,0 +1,4 @@
+export default function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+}
